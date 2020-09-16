e8fb35ba490be437d364f8a612f074579528d1d6
SachinMaharana <sachin.nicky@gmail.com>
2020-09-17 00:11:31 +0530

    bubble up: noisy

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
-use std::env;
+use std::collections::HashMap;
 
 fn main() {
-   let port = env::var("PORT").unwrap_or("3000".to_string());
-   println!("{}", port);
+    match get_current_year() {
+        Ok(date) => println!("We have time travelled! {}", date),
+        Err(e) => eprintln!("Oh Noes, we don't know {}", e)
+    }
+  
 }
+
+fn get_current_year() -> Result<String, reqwest::Error> {
+    let url = "https://postman-echo.com/time/object";
+    let result = reqwest::blocking::get(url);
+
+    let response = match result {
+        Ok(res) => res,
+        Err(err) => return Err(err)
+    };
+
+
+    let body = response.json::<HashMap<String, i32>>();
+
+    let json = match body {
+        Ok(json) => json,
+        Err(err) => return Err(err),
+    };
+
+    let date = json["years"].to_string();
+
+    Ok(date)
+}
+
