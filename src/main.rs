a1face1c4bbb264adbc31abb544c45e75e307f75
SachinMaharana <sachin.nicky@gmail.com>
2020-09-16 23:59:37 +0530

    terminate the error

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
 fn main() {
-    let content = fs::read_to_string("./Cargo.toml").unwrap();
+    let content = fs::read_to_string("./Carggo.toml").expect("Can't read toml file");
     println!("{}", content);
 }
