d3dd0943069becc6a7471e6eb6e2397ee759bfdd
SachinMaharana <sachin.nicky@gmail.com>
2020-09-17 00:18:19 +0530

    error: different kinds of error

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use chrono::NaiveDate;
 
 fn main() {
     match get_current_year() {
@@ -12,7 +13,11 @@ fn get_current_year() -> Result<String, reqwest::Error> {
     let url = "https://postman-echo.com/time/object";
     let res = reqwest::blocking::get(url)?.json::<HashMap<String, i32>>()?;
 
-    let date = res["years"].to_string();
+    // let date = res["years"].to_string();
+
+    let formatted_date =  format!("{}-{}-{}", res["years"], res["months"] + 1, res["date"]);
+    let parsed_date = NaiveDate::parse_from_str(formatted_date.as_str(), "%Y-%m-%d")?;
+    let date = parsed_date.format("%Y %B %d").to_string();
 
     Ok(date)
 }
