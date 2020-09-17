dd5429960b73711fda548f69e96aa631d13f4740
SachinMaharana <sachin.nicky@gmail.com>
2020-09-17 17:40:05 +0530

    add main error handling

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,21 @@ fn main() {
     match get_current_year() {
         Ok(date) => println!("We have time travelled! {}", date),
         // Err(e) => eprintln!("Oh Noes, we don't know {}", e)
-        Err(e) => {
-            eprintln!("Oh Noes, we don't know");
-            if let Some(err) = e.downcast_ref::<reqwest::Error>() {
-                eprintln!("Request Error: {}", err)
-            } else if let Some(err) = e.downcast_ref::<chrono::format::ParseError>() {
-                eprintln!("Parse Error: {}", err)
-            }
-        }
+        Err(e) => match e {
+            MyCustomError::HttpError => eprintln!("Request Error: {}", e),
+            MyCustomError::ParseError => eprintln!("Parse Error: {}", e),
+        },
     }
 }
 
 fn get_current_year() -> Result<String, MyCustomError> {
     let url = "https://postman-echo.com/time/object";
-    let res = reqwest::blocking::get(url)?
-        .json::<HashMap<String, i32>>()?;
+    let res = reqwest::blocking::get(url)?.json::<HashMap<String, i32>>()?;
     // let date = res["years"].to_string();
 
     let formatted_date = format!("{}-{}-{}", res["years"], res["months"] + 1, res["date"]);
     let parsed_date = NaiveDate::parse_from_str(formatted_date.as_str(), "%Y-%m-%d")?;
-        
+
     let date = parsed_date.format("%Y %B %d").to_string();
 
     Ok(date)
